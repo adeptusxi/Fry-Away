054760b0dd9420dcaca98fe653e268a62aa577a7
clytieq <113843835+clytieq@users.noreply.github.com>
2026-09-13 19:39:58 -0400

    constant forces on frisbee: lift, drag, gravitational
    needs testing & tweaking

--- Assets/Scripts/Throw/ThrowPhysicsFrisbee.cs
+++ Assets/Scripts/Throw/ThrowPhysicsFrisbee.cs
@@ -2,6 +2,37 @@ using UnityEngine;
 
 public class ThrowPhysicsFrisbee : ThrowPhysics
 {
+    [Tooltip("The object travels at its release velocity multiplied by this. Basically \"throw power\"")]
+    [SerializeField] private float velocityScalar = 1f;
+    [SerializeField] private float rho = 1.2f; // air density in kg/m^3;usually ~1.2 kg/m^3 for dry air
+    [Tooltip("Initial lift coefficient (try 0.13)")]
+    [SerializeField] private float cli;
+    // TODO: add c_lift_aoa once changing aoa is implemented
+    [Tooltip("Initial drag coefficient (try 0.085)")]
+    [SerializeField] private float cdi;
+    [Tooltip("Frisbee radius (m) to approximate wing area")]
+    [SerializeField] private float r;
+    [Tooltip("Frisbee mass (kg)")]
+    [SerializeField] private float m;
+
+    private float liftdragConstant; // rho * a/2 --> |L| = cl * V^2 * liftdragConstant
+    private float aoa; // angle of attack (rad)
+    // TODO: Clytie - implement changing aoa affecting lift & drag
+    //private float liftMagnitude;
+    //private float dragMagnitude;
+    private Vector3 lift; // (N)
+    private Vector3 drag; // (N)
+    private Vector3 fg; // gravitational force (N)
+
+    protected override void Begin()
+    {
+        liftdragConstant = rho * Mathf.PI * Mathf.Pow(r, 2f) / 2f;
+        aoa = -Data.hand.rotation.eulerAngles.x; // neg sign from LHR
+        lift = cli * Mathf.Pow(CurrentVelocity.magnitude, 2f) * liftdragConstant * Vector3.up;
+        drag = - lift * cdi / cli;
+        fg = m * 9.81f * Vector3.down;
+    }
+
     protected override void Step()
     {
         /* TODO: Clytie - implement frisbee physics (gravity, lift, drag, tilt, etc.)
@@ -9,6 +40,8 @@ public class ThrowPhysicsFrisbee : ThrowPhysics
          * the Data variable from the parent ThrowPhysics.cs class holds all the information about the frisbee and the hand. 
          * a dummy example is in ThrowPhysicsLinear.cs 
          */
+        Vector3 dv = (lift + drag + fg) * Time.deltaTime / m;
+        TryMove(CurrentVelocity * Time.deltaTime, Target.rotation);
     }
 
     protected override void Stop()
