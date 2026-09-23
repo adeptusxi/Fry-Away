e945ff1531b411eb997597c26c02df0370627038
Clytie Qiu <clytieq@andrew.cmu.edu>
2026-09-22 23:34:02 -0500

    clean up bread frisbee debug gizmos
    [git-p4: depot-paths = "//depot/IDeATe/courses/2026_fall/53451_RIGD/TeamFNF/Fry Away/": change = 7871]

--- Assets/Scripts/Throw/FixedGripTransformer.cs
+++ Assets/Scripts/Throw/FixedGripTransformer.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 public class FixedGripTransformer : MonoBehaviour, ITransformer
 {
     [SerializeField] private float h = 0.05f; // gizmo scale
-    [Tooltip("Where the object sits relative to the grabbing hand, in the hand's local space.")]
-    [SerializeField] private Transform heldPositionOffsetTransform;
-    [SerializeField] private Vector3 heldPositionOffset;
+    [Tooltip("Where and what orientation the object sits relative to the grabbing hand, in the hand's local space.")]
+    [SerializeField] private Transform heldOffsetTransform;
+    private Vector3 heldPositionOffset;
 
-    [Tooltip("How the object is oriented relative to the grabbing hand, in degrees.")]
-    [SerializeField] private Quaternion heldRotationOffset;
+    //[Tooltip("How the object is oriented relative to the grabbing hand, in degrees.")]
+    private Quaternion heldRotationOffset;
 
     // this should be set by ThrowInteractable during a throw
     // (so while the interactor takes a few frames to "notice" the object has been released, the grip does not bring the object back to the hand) 
@@ -22,8 +22,8 @@ public class FixedGripTransformer : MonoBehaviour, ITransformer
     public void Initialize(IGrabbable grabbable)
     {
         this.grabbable = grabbable;
-        heldRotationOffset = heldPositionOffsetTransform.localRotation;
-        heldPositionOffset = heldPositionOffsetTransform.localPosition;
+        heldRotationOffset = heldOffsetTransform.localRotation;
+        heldPositionOffset = heldOffsetTransform.localPosition;
     }
 
     public void BeginTransform()
@@ -37,8 +37,8 @@ public class FixedGripTransformer : MonoBehaviour, ITransformer
         {
             return;
         }
-        heldRotationOffset = heldPositionOffsetTransform.localRotation;
-        heldPositionOffset = heldPositionOffsetTransform.localPosition;
+        heldRotationOffset = heldOffsetTransform.localRotation;
+        heldPositionOffset = heldOffsetTransform.localPosition;
 
         Pose grabPose = grabbable.GrabPoints[0];
         grabbable.Transform.SetPositionAndRotation(
@@ -51,10 +51,10 @@ public class FixedGripTransformer : MonoBehaviour, ITransformer
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
-        Gizmos.DrawWireCube(heldPositionOffset, new Vector3(3*h, h, 3*h));
+        Gizmos.DrawSphere(Vector3.zero, h);
         Gizmos.color = Color.red;
         Matrix4x4 old = Gizmos.matrix;
-        Matrix4x4 handMatrix = grabbable == null ? heldPositionOffsetTransform.localToWorldMatrix : grabbable.Transform.localToWorldMatrix;
+        Matrix4x4 handMatrix = grabbable == null ? heldOffsetTransform.localToWorldMatrix : grabbable.Transform.localToWorldMatrix;
         Gizmos.matrix = handMatrix;
         Gizmos.DrawWireCube(Vector3.zero, new Vector3(3*h, h, 3*h));
         Gizmos.matrix = old;
